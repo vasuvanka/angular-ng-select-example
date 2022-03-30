0391be0b611e242babf25ec4d9917b7a80e6063b
vasuvanka <vanka.vasu@gmail.com>
2022-03-30 23:58:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-loypu2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-loypu2)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  selectedCar: number;
+
+  cars = [
+    { id: 1, name: 'Volvo' },
+    { id: 2, name: 'Saab' },
+    { id: 3, name: 'Opel' },
+    { id: 4, name: 'Audi' },
+  ];
+
+  customSearchFn(term: string, item: any) {
+    term = term.toLowerCase();
+    return (
+      item.id.toLowerCase().indexOf(term) > -1 ||
+      item.name.toLowerCase() === term
+    );
+  }
+}
