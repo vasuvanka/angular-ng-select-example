a019a7df3fee7d64a932620f86873e18cceeb183
vasuvanka <vanka.vasu@gmail.com>
2022-03-31 00:06:16 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
   selectedCar: number;
 
@@ -16,10 +16,15 @@ export class AppComponent {
     { id: 4, name: 'Audi' },
   ];
 
+  ngOnInit() {
+    console.log('hello ' + this.cars.length);
+  }
+
   customSearchFn(term: string, item: any) {
     term = term.toLowerCase();
     return (
-      item.id.toLowerCase().indexOf(term) > -1 ||
+      item.id.toString() == term ||
+      item.name.toLowerCase().indexOf(term) > -1 ||
       item.name.toLowerCase() === term
     );
   }
